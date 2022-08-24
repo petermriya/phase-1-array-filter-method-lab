93606cd17ce7b2e05519ac6ebe6dba12759d1f4c
petermriya <petermuriya@gmail.com>
2022-08-24 15:31:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(find, found){
+    return find.filter(
+        (couldMatch) => couldMatch.toLowerCase() === found.toLowerCase()
+    );
+}
+
+function fuzzyMatch(seek, sought) {
+    return seek.filter( (possibleMatch) => possibleMatch.toLowerCase().indexOf(sought.toLowerCase()) === 0
+
+    );
+}
+function matchName(source, soughtName) {
+    return source.filter((record) => record.name === soughtName);
+}
